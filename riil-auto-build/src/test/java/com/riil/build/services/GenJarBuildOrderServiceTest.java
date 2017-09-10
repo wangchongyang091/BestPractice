5e5c134d16580000f27d52f8c7b1ab1234d6acb0
wangchongyang091 <136765858@qq.com>
2017-09-11 04:44:44 +0800

    提高抽象层次

--- riil-auto-build/src/test/java/com/riil/build/services/GenJarBuildOrderServiceTest.java
+++ riil-auto-build/src/test/java/com/riil/build/services/GenJarBuildOrderServiceTest.java
@@ -1,11 +1,10 @@
 package com.riil.build.services;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.riil.build.pojo.JarDirectPojo;
-import com.riil.build.pojo.JarPojo;
+import com.riil.build.pojo.BasicBuildPojo;
+import com.riil.build.pojo.BasicBuildRelationPojo;
 import java.io.File;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +16,8 @@ import org.junit.Test;
  * User: wangchongyang on 2017/9/8 0008.
  */
 public class GenJarBuildOrderServiceTest {
+
+
     private static final String RIIL_WEBFRAMEWORK_PATH = "E:\\codeSVN\\RIIL_WebFramework";
     private static final String TRUNK_SOURCE = "\\trunk\\source";
     private static final String superRiilPomPath = "D:\\m2_repository\\com\\riil\\super.riil.pom\\6.8.7-SNAPSHOT\\super.riil.pom-6.8.7-SNAPSHOT.pom";
@@ -44,25 +45,32 @@ public class GenJarBuildOrderServiceTest {
     public void getPomsBySpecifyRange() throws Exception {
         final List<File> pomList = buildOrderService.getPomsBySpecifyRange(RIIL_WEBFRAMEWORK_PATH + TRUNK_SOURCE);
         for (File file : pomList) {
-            System.out.println(file.getCanonicalPath()/*+":"+file.getParent()*/);
+            System.out.println(file.getCanonicalPath() + ":" + file.getParent());
         }
     }
 
     @Test
+    public void genProjectBuildPathRegister() throws Exception {
+        final Map<String, String> buildPathRegister = buildOrderService.genProjectBuildPathRegister(buildOrderService.getPomsBySpecifyRange(RIIL_WEBFRAMEWORK_PATH + TRUNK_SOURCE));
+        System.out.println(buildPathRegister);
+    }
+
+    @Test
     public void getDirectDependencyByPom() throws Exception {
-        final JarDirectPojo jarDirectPojo = buildOrderService.getDirectDependencyByPom(EAC_ACTION_POM_PATH);
-        System.out.println(jarDirectPojo);
+        final List<File> pomList = buildOrderService.getPomsBySpecifyRange(RIIL_WEBFRAMEWORK_PATH + TRUNK_SOURCE);
+        final Set<BasicBuildRelationPojo> relationPojos = buildOrderService.getBuildRelations(pomList);
+        System.out.println(relationPojos);
     }
 
     @Test
     public void getJarBuildOrder() throws Exception {
-        List<JarDirectPojo> jarDirectPojos = mockJarDirectPojoList();
-        final List<Set<JarPojo>> jarBuildOrder = buildOrderService.getJarBuildOrder(jarDirectPojos, NEO4J_DATA_PATH);
+        Set<BasicBuildRelationPojo> jarDirectPojos = mockJarDirectPojoList();
+        final List<Set<BasicBuildPojo>> jarBuildOrder = buildOrderService.getJarBuildOrder(jarDirectPojos, NEO4J_DATA_PATH);
         System.out.println(jarBuildOrder);
     }
 
-    private List<JarDirectPojo> mockJarDirectPojoList() {
-        List<JarDirectPojo> jarDirectPojos = Lists.newArrayList();
+    private Set<BasicBuildRelationPojo> mockJarDirectPojoList() {
+        Set<BasicBuildRelationPojo> jarDirectPojos = Sets.newHashSet();
         Set<String> nodeNameSet = ImmutableSet.of("A", "B", "C", "D", "E", "F", "G", "H", "I");
         Map<String, Set<String>> nodeRelationMap = Maps.newHashMap();
         nodeRelationMap.put("A", ImmutableSet.of("B", "G"));
@@ -74,28 +82,26 @@ public class GenJarBuildOrderServiceTest {
         nodeRelationMap.put("G", ImmutableSet.of("D"));
         nodeRelationMap.put("H", ImmutableSet.of("C", "G", "I"));
         nodeRelationMap.put("I", ImmutableSet.of("E"));
-        Map<String, JarDirectPojo> jarDirectPojoMap = Maps.newHashMap();
+        Map<String, BasicBuildRelationPojo> jarDirectPojoMap = Maps.newHashMap();
         for (String nodeName : nodeNameSet) {
-            final JarDirectPojo jarDirectPojo = genJarDirectPojo(nodeName);
+            final BasicBuildRelationPojo jarDirectPojo = getBuildRelation(nodeName);
             jarDirectPojoMap.put(nodeName, jarDirectPojo);
         }
         for (Map.Entry<String, Set<String>> setEntry : nodeRelationMap.entrySet()) {
             final Set<String> dependencyNodes = setEntry.getValue();
-            Set<JarPojo> jarDirectPojoSet = Sets.newHashSet();
+            Set<BasicBuildPojo> jarDirectPojoSet = Sets.newHashSet();
             for (String nodeName : dependencyNodes) {
                 jarDirectPojoSet.add(jarDirectPojoMap.get(nodeName));
             }
-            jarDirectPojoMap.get(setEntry.getKey()).setDirectDependency(jarDirectPojoSet);
+            jarDirectPojoMap.get(setEntry.getKey()).setBeforeBuilds(jarDirectPojoSet);
         }
         jarDirectPojos.addAll(jarDirectPojoMap.values());
         return jarDirectPojos;
     }
 
-    private JarDirectPojo genJarDirectPojo(String nodeName) {
-        final JarDirectPojo jarDirectPojo = new JarDirectPojo();
-        jarDirectPojo.setGroupId("groupId-" + nodeName);
-        jarDirectPojo.setArtifactId("artifactId-" + nodeName);
-        jarDirectPojo.setVersion("version-" + nodeName);
+    private BasicBuildRelationPojo getBuildRelation(String buildPath) {
+        final BasicBuildRelationPojo jarDirectPojo = new BasicBuildRelationPojo();
+        jarDirectPojo.setBuildPath(buildPath);
         return jarDirectPojo;
     }
 
